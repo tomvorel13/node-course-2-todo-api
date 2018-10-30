23207583a0f1a862ae854810652ad05274677965
Tom Vorel <tomvorel13@gmail.com>
2018-10-30 16:47:32 +0100

    Has user password

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,16 +1,32 @@
 const { SHA256 } = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-let data = {
-  id: 10
-};
+let password = '123abc!';
 
-let token = jwt.sign(data, '123abc');
-console.log(token);
+bcrypt.genSalt(10, (err, salt) => {
+  bcrypt.hash(password, salt, (err, hash) => {
+    console.log(hash);
+  });
+});
 
-let decoded = jwt.verify(token, '123abc');
+let hashedPassword =
+  '2a$10$ZQuAYkP5.ZX2UXMSxi0r2.Q5NWh8r0Kx43nu4CUArYRfoby1MdIqW';
 
-console.log(decoded);
+bcrypt.compare(password, hashedPassword, (err, res) => {
+  console.log(res);
+});
+
+// let data = {
+//   id: 10
+// };
+
+// let token = jwt.sign(data, '123abc');
+// console.log(token);
+
+// let decoded = jwt.verify(token, '123abc');
+
+// console.log(decoded);
 
 // let message = 'Deez nuts is the funniest video ever.';
 // const hash = SHA256(message).toString();
